6fbd4eb82dde5edba2f47ec62ebea76aa384df1d
DavideViolante <fungtq3@gmail.com>
2020-11-09 18:38:50 +0100

    First version of nodejs wrapper for ocr.space

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# ocr-space-api-wrapper
-Node.js wrapper for ocr.space APIs.
+# OCR Space Node.js API wrapper
+Node.js wrapper for [ocr.space APIs](https://ocr.space/ocrapi).
+
+### Install
+`npm i ocr-space-api-wrapper`
+
+### Usage
+```js
+const ocrSpace = require('ocr-space-api-wrapper')
+
+async function main () {
+  // Using the OCR.space default free token
+  const response1 = await ocrSpace('http://dl.a9t9.com/ocrbenchmark/eng.png')
+  // Using your personal token + a local file
+  const response2 = await ocrSpace('/path/to/file.pdf', { apiKey: '<API_KEY_HERE>' })
+  // Using your personal token + a base64image + custom language
+  const response2 = await ocrSpace('data:image/png;base64,superLongStringHere...', { apiKey: '<API_KEY_HERE>', language: 'ita' })
+}
+```
+
+### Params
+#### Input: string (required)
+The input param specifies the input file (see example above). It can be one of the following:
+ - a remote `URL address`;
+ - a local `file path`;
+ - a `base64 image` string.
+
+#### Options: object (not required)
+This param is an object with the following keys:
+- `apiKey`: your API key for [ocr.space APIs](https://ocr.space/ocrapi).
+- `ocrUrl`: a different URL for oce.space APIs, for example when you purchase the PRO plans
+- All other params as documented in the [official website](https://ocr.space/OCRAPI#Response).
+
+### Response
+Refer to the [official website](https://ocr.space/OCRAPI#Response).
+
+### Author
+- [Davide Violante](https://github.com/DavideViolante)
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require('fs')
+const axios = require('axios')
+const FormData = require('form-data')
+
+function detectInput (input) {
+  if (input.startsWith('http')) return 'url'
+  if (input.startsWith('data:')) return 'base64Image'
+  return 'file'
+}
+
+/**
+ *
+ * @param {string} input Input file as url, file path or base64 image (required)
+ * @param {object} options Options
+ */
+async function ocrSpace (input, options = {}) {
+  try {
+    const {
+      apiKey,
+      ocrUrl,
+      language,
+      isOverlayRequired,
+      filetype,
+      detectOrientation,
+      isCreateSearchablePdf,
+      isSearchablePdfHideTextLayer,
+      scale,
+      isTable,
+      OCREngine
+    } = options
+    const data = new FormData()
+    const detectedInput = detectInput(input)
+    switch (detectedInput) {
+      case 'file':
+        data.append('file', fs.createReadStream(input))
+        break
+      case 'url':
+      case 'base64Image':
+        data.append(detectedInput, input)
+        break
+    }
+    data.append('language', language || 'eng')
+    data.append('isOverlayRequired', isOverlayRequired || 'false')
+    if (filetype) {
+      data.append('filetype', filetype)
+    }
+    data.append('detectOrientation', detectOrientation || 'false')
+    data.append('isCreateSearchablePdf', isCreateSearchablePdf || 'true')
+    data.append('isSearchablePdfHideTextLayer', isSearchablePdfHideTextLayer || 'true')
+    data.append('scale', scale || 'false')
+    data.append('isTable', isTable || 'false')
+    data.append('OCREngine', OCREngine || '1')
+
+    const request = {
+      method: 'POST',
+      url: ocrUrl || 'https://api.ocr.space/parse/image',
+      headers: {
+        apikey: apiKey || 'helloworld',
+        ...data.getHeaders()
+      },
+      data
+    }
+    const response = await axios(request)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    /*
+    {
+      ParsedResults: [],
+      OCRExitCode: 6,
+      IsErroredOnProcessing: true,
+      ErrorMessage: [ 'Timed out waiting for results' ],
+      ProcessingTimeInMilliseconds: '49024'
+    }
+    */
+    console.error(error)
+  }
+}
+
+module.exports = ocrSpace
