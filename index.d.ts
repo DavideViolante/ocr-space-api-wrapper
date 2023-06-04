26aa373cd823933520bdc50d9332cc5b9dc203ae
DavideViolante <fungtq3@gmail.com>
2023-06-04 11:53:22 +0200

    Add typings to options param

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,17 @@
+type Languages = 'ara' | 'bul' | 'chs' | 'cht' | 'hrv' | 'cze' | 'dan' | 'dut' |
+  'eng' | 'fin' | 'fre' | 'ger' | 'gre' | 'hun' | 'kor' | 'ita' | 'jpn' | 'pol' |
+  'por' | 'rus' | 'slv' | 'spa' | 'swe' | 'tur' | 'hin' | 'kan' | 'per' | 'tel' |
+  'tam' | 'tai' | 'vie';
+
+type FileTypes = 'PDF' | 'GIF' | 'PNG' | 'JPG' | 'TIF' | 'BMP';
+
+type Options = {
+  apiKey?: string; ocrUrl?: string; language?: Languages; isOverlayRequired?: boolean;
+  filetype?: FileTypes; detectOrientation?: boolean; isCreateSearchablePdf?: boolean;
+  isSearchablePdfHideTextLayer?: boolean; scale?: boolean; isTable?: boolean;
+  OCREngine?: '1' | '2' | '3';
+}
+
 declare module "ocr-space-api-wrapper" {
-    export function ocrSpace(input: string, options?: any): any;
+  export function ocrSpace(input: string, options?: Options): any;
 }
