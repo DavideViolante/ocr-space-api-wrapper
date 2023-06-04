54217d50e62061c02b9d95b2a4493fd92e8d44dd
DavideViolante <fungtq3@gmail.com>
2023-06-04 14:03:33 +0200

    Rename types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-type OcrSpaceLanguages = 'ara' | 'bul' | 'chs' | 'cht' | 'hrv' | 'cze' | 'dan' | 'dut' |
+type Languages = 'ara' | 'bul' | 'chs' | 'cht' | 'hrv' | 'cze' | 'dan' | 'dut' |
   'eng' | 'fin' | 'fre' | 'ger' | 'gre' | 'hun' | 'kor' | 'ita' | 'jpn' | 'pol' |
   'por' | 'rus' | 'slv' | 'spa' | 'swe' | 'tur' |
   // The following are only supported by OCREngine = '3'
   'hin' | 'kan' | 'per' | 'tel' | 'tam' | 'tai' | 'vie';
 
-type OcrSpaceFileTypes = string | 'PDF' | 'GIF' | 'PNG' | 'JPG' | 'TIF' | 'BMP';
+type FileTypes = string | 'PDF' | 'GIF' | 'PNG' | 'JPG' | 'TIF' | 'BMP';
 
 type ParsedResult = {
   ErrorMessage: string;
@@ -29,9 +29,9 @@ type OcrSpaceResponse = {
 type OcrSpaceOptions = {
   apiKey?: string;
   ocrUrl?: string;
-  language?: OcrSpaceLanguages;
+  language?: Languages;
   isOverlayRequired?: boolean;
-  filetype?: OcrSpaceFileTypes;
+  filetype?: FileTypes;
   detectOrientation?: boolean;
   isCreateSearchablePdf?: boolean;
   isSearchablePdfHideTextLayer?: boolean;
@@ -41,7 +41,7 @@ type OcrSpaceOptions = {
 }
 
 declare module "ocr-space-api-wrapper" {
-  export type OcrSpaceLanguages = OcrSpaceLanguages;
-  export type OcrSpaceFileTypes = OcrSpaceFileTypes;
+  export type OcrSpaceLanguages = Languages;
+  export type OcrSpaceFileTypes = FileTypes;
   export function ocrSpace(input: string, options?: OcrSpaceOptions): OcrSpaceResponse;
 }
